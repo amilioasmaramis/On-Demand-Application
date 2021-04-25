35f9712c232e904795b235c85e25615f648a2707
Amilio Asmaramis <amilio.asmaramis@gmail.com>
2021-04-25 19:52:43 +0700

    First Commit Setting Db Postgres

---
+++ config/constants.js
@@ -0,0 +1,15 @@
+const DB_USERNAME_NAME = process.env.DB_DATABASE_NAME
+const DB_PASSWORD_NAME = process.env.DB_PASSWORD
+const DB_DATABASE_NAME = process.env.DB_DATABASE_NAME
+const DB_HOST_NAME = process.env.DB_HOST_NAME
+const DB_DIALECT_NAME = process.env.DB_DIALECT_NAME
+const JWT_SECRET = process.env.JWT_SECRET
+
+module.exports = { 
+  DB_USERNAME_NAME,
+  DB_PASSWORD_NAME,
+  DB_DATABASE_NAME,
+  DB_HOST_NAME,
+  DB_DIALECT_NAME,
+  JWT_SECRET
+}
